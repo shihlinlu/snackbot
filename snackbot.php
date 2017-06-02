1ea4ddf5cba9a2db649c26ced7a4dbe9369faf24
Shihlin Lu <shihlinlu@gmail.com>
2017-06-02 11:27:49 -0600

    setup tea, coffee, bagel, and help commands

--- snackbot.php
+++ snackbot.php
@@ -3,77 +3,64 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require 'vendor/autoload.php';
 use PhpSlackBot\Bot;
 
-// custom command
-class MyCommand extends \PhpSlackBot\Command\BaseCommand {
-
-	//private $count = 0;
-	private $initiator;
-	private $scores = array();
-	private $status = 'free';
+/**
+ * Tea command
+ */
+class TeaCommand extends \PhpSlackBot\Command\BaseCommand {
 
 	protected function configure() {
-		$this->setName('!snack');
+		$this->setName('/tea');
 	}
 
 	protected function execute($message, $context) {
-		$args = $this->getArgs($message);
-		$command = isset($args[1]) ? $args[1] : '';
-
-		switch($command) {
-			case 'tea':
-				$this->tea($args);
-				break;
-			case 'status':
-				$this->status();
-				break;
-			default:
-				$this->send($this->getCurrentChannel(), $this->getCurrentUser(),'Error!!!. Use "'.$this->getName().' tea" or "'.$this->getName().' coffee"');
-
-		}
+		$this->send($this->getCurrentChannel(), null, "What kind of tea do you prefer? :tea-pot:");
 	}
 
-	/**
-	 * Tea function
-	 */
-	private function tea($args) {
-		if ($this->status == 'free') {
-			$this->subject = isset($args[2]) ? $args[2] : null;
-			if (!is_null($this->subject)) {
-				$this->subject = str_replace(array('<', '>'), '', $this->subject);
-			}
-			$this->status = 'running';
-			$this->initiator = $this->getCurrentUser();
-			$this->scores = array();
-			$this->send($this->getCurrentChannel(), null,
-				'The snack timer has been started by '.$this->getUserNameFromUserId($this->initiator)."\n".
-				'Please vote'.(!is_null($this->subject) ? ' for '.$this->subject : ''));
-			$this->send($this->getCurrentChannel(), $this->getCurrentUser(), 'Use "'.$this->getName().' done" to end the session');
-		}
-		else {
-			$this->send($this->getCurrentChannel(), $this->getCurrentUser(), 'A tea session is still active');
-		}
+}
+
+/**
+ * Coffee command
+ */
+
+class CoffeeCommand extends \PhpSlackBot\Command\BaseCommand {
+
+	protected function configure() {
+		$this->setName('/coffee');
 	}
 
-	private function status() {
-		$message = 'Current status : '.$this->status;
-		if ($this->status == 'running') {
-			$message .= "\n".'Initiator : '.$this->getUserNameFromUserId($this->initiator);
-		}
-		$this->send($this->getCurrentChannel(), null, $message);
-		if ($this->status == 'running') {
-			if (empty($this->scores)) {
-				$this->send($this->getCurrentChannel(), null, 'Your snack is not ready yet!');
-			}
-			else {
-				$message = '';
-				foreach ($this->scores as $user => $score) {
-					$message .= $this->getUserNameFromUserId($user).' has voted'."\n";
-				}
-				$this->send($this->getCurrentChannel(), null, $message);
-			}
-		}
+	protected function execute($message, $context) {
+		$this->send($this->getCurrentChannel(), null, "What kind of coffee do you prefer? :coffee:");
 	}
 
+}
+
+/**
+ * Bagel command
+ */
+class BagelCommand extends \PhpSlackBot\Command\BaseCommand {
+
+	protected function configure() {
+		$this->setName('/bagel');
+	}
+
+	protected function execute($message, $context) {
+		$this->send($this->getCurrentChannel(), null, "What kind of tea do you prefer? :bread:");
+	}
+
+}
+
+/**
+ * Help command lists all possible commands to interact with the bot
+ */
+class HelpCommand extends \PhpSlackBot\Command\BaseCommand {
+
+	protected function configure() {
+		$this->setName('/help');
+	}
+
+	protected function execute($message, $context) {
+		$this->send($this->getCurrentChannel(), null, "What can snackbot do for you? /n tea, coffee, bagel...");
+	}
 }
 
 //grab mySQL statement
@@ -84,7 +71,10 @@ $slack = $config["slack"];
 
 $bot = new Bot();
 $bot->setToken($slack);
-$bot->loadCommand(new MyCommand());
+$bot->loadCommand(new TeaCommand());
+$bot->loadCommand(new CoffeeCommand());
+$bot->loadCommand(new BagelCommand());
+$bot->loadCommand(new HelpCommand());
 $bot->loadInternalCommands(); // this loads example commands
 
 // active messaging: sends messages to users without the need for them to send one first
